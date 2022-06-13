e63ee2687b597c08ea3248e2561d22108b8dfbf2
Aolia rachman <aoliarachman150@gmail.com>
2022-06-13 17:57:53 +0700

    first commit

---
+++ router_pattern_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRouterPatternNamedParamater(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/products/:id/items/:itemid", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		id := params.ByName("id")
+		itemId := params.ByName("itemId")
+		text := "Product" + id + itemId
+		fmt.Fprint(writer, text)
+	})
+
+	request := httptest.NewRequest("GET", "http://localhost:3000/products/1/items/1", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "product 1 item 1", string(body))
+}
+
+func TestRouterPatternCatchAllParameter(t *testing.T) {
+
+	router := httprouter.New()
+	router.GET("/image/*image", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		image := params.ByName("image")
+		text := "Image : " + image
+		fmt.Fprint(writer, text)
+	})
+
+	request := httptest.NewRequest("GET", "http://localhost:3000/image/small/profile.png", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Image : /small/profile.png", string(body))
+}
